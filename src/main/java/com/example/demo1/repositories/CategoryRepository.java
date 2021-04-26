08cbe91a706413122432f27356500d40778a42e6
SanaJava <sana.eneroth@gmail.com>
2021-04-27 01:09:30 +0200

    Reformat code and clean-up

--- src/main/java/com/example/demo1/repositories/CategoryRepository.java
+++ src/main/java/com/example/demo1/repositories/CategoryRepository.java
@@ -1,20 +1,10 @@
 package com.example.demo1.repositories;
 
 import com.example.demo1.models.Category;
-import com.example.demo1.models.Customer;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
 
-/**
- * Created by Henrik Elofsson
- * Date: 2021-04-16
- * Time: 14:46
- * Project: Project2003-G07
- * Copyright: MIT
- */
 public interface CategoryRepository extends CrudRepository<Category, Long> {
-
     List<Category> findByNamn(String namn);
-
 }
