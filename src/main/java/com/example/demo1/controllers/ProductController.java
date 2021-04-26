d703b45ec1cfda469df017aac8851051a924b9ef
SanaJava <sana.eneroth@gmail.com>
2021-04-27 00:58:49 +0200

    Reformat code and clean-up

--- src/main/java/com/example/demo1/controllers/ProductController.java
+++ src/main/java/com/example/demo1/controllers/ProductController.java
@@ -7,13 +7,6 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
 
-/**
- * Created by Salah Abdinoor
- * 4/16/2021
- * 4:27 PM
- * Project2003-G073
- * Copyright: MIT
- */
 @RestController
 @RequestMapping(path = "/product/")
 public class ProductController {
@@ -24,50 +17,47 @@ public class ProductController {
     /**
      * Adds product to database, ID is auto-incremented
      * checks to see that there is not already an existing version of the same product
+     *
      * @return String
      */
 
     @PostMapping(path = "/add")
-    public @ResponseBody String addProduct(
+    public @ResponseBody
+    String addProduct(
             @RequestParam String name,
-            @RequestParam (required = false) int storage,
-            @RequestParam (required = false) String image,
-            @RequestParam (required = false) String description,
-            @RequestParam (required = false) int price,
-            @RequestParam  String category,
-            @RequestParam  boolean isVisible ){
+            @RequestParam(required = false) int storage,
+            @RequestParam(required = false) String image,
+            @RequestParam(required = false) String description,
+            @RequestParam(required = false) int price,
+            @RequestParam String category,
+            @RequestParam boolean isVisible) {
 
         System.out.println("YO");
 
-        Product product = new Product(name,storage, image, description, price,category,isVisible);
+        Product product = new Product(name, storage, image, description, price, category, isVisible);
 
-        if ((productRepository.findByName(name).size()!=0)){
+        if ((productRepository.findByName(name).size() != 0)) {
 
             return "product already exists, use put method instead";
-        }else {
+        } else {
             productRepository.save(product);
         }
 
-
         return "Product Saved";
-
     }
 
     // Ändra så att produkten bara gömmer sid! Test ATM
 
     @DeleteMapping(path = "/delete")
-    public @ResponseBody String deleteProduct(@RequestParam Long id){
-
-
-
-        if ((productRepository.findById(id).isEmpty())){
+    public @ResponseBody
+    String deleteProduct(@RequestParam Long id) {
+        if ((productRepository.findById(id).isEmpty())) {
             return "Product doesn't exist";
-        }else {
+        } else {
 
             productRepository.delete(productRepository.findById(id).get());
             return "Product deleted";
         }
-
 
         /**
          * Shows all products
@@ -75,50 +65,49 @@ public class ProductController {
          * @param
          * @return
          */
-
     }
 
     //localhost:1234/product/showProducts
     @GetMapping(path = "/showAll")
-    public @ResponseBody Iterable<Product> showProducts(){
+    public @ResponseBody
+    Iterable<Product> showProducts() {
 
         return productRepository.findAll();
-
     }
 
     /**
      * Shows product from id
+     *
      * @param id
      * @return
      */
 
     @GetMapping(path = "/showProductById")
-    public @ResponseBody Optional<Product> showProductById(Long id){
+    public @ResponseBody
+    Optional<Product> showProductById(Long id) {
 
         return productRepository.findById(id);
-
     }
 
     @PutMapping(path = "/update")
-    public @ResponseBody String updateProduct(
+    public @ResponseBody
+    String updateProduct(
             @RequestParam Long id,
-            @RequestParam (required = false) String name,
-            @RequestParam (required = false) int storage,
-            @RequestParam (required = false) String image,
-            @RequestParam (required = false) String description,
-            @RequestParam (required = false) int price,
-            @RequestParam (required = false) String category,
-            @RequestParam (required = false) boolean isVisible ){
+            @RequestParam(required = false) String name,
+            @RequestParam(required = false) int storage,
+            @RequestParam(required = false) String image,
+            @RequestParam(required = false) String description,
+            @RequestParam(required = false) int price,
+            @RequestParam(required = false) String category,
+            @RequestParam(required = false) boolean isVisible) {
 
         var putId = productRepository.findById(id);
 
-        if (productRepository.findById(id).isEmpty()){
-
+        if (productRepository.findById(id).isEmpty()) {
             return "There is no product with that ID";
-        }
-        else {
 
-            var temp =  productRepository.findById(id).get();
+        } else {
+            var temp = productRepository.findById(id).get();
 
             temp.setName(name);
             temp.setStorage(storage);
@@ -131,7 +120,6 @@ public class ProductController {
             productRepository.save(temp);
 
             return "Product updated";
-
         }
     }
 }
