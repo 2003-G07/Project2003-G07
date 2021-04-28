893548d82b78182338f28b420ae5559093154d62
Salah Abdinoor <Salah.abdinoor@hotmail.com>
2021-04-28 14:30:06 +0200

    Beställnings api på G

---
+++ src/main/java/com/example/demo1/util/APILayout.java
@@ -0,0 +1,18 @@
+package com.example.demo1.util;
+
+/**
+ * Created by Salah Abdinoor
+ * 4/28/2021
+ * 2:04 PM
+ * Heroku
+ * Copyright: MIT
+ */
+
+/**
+ * This class for the Order API to present the JSON file to Hakim in proper format..
+ *
+ */
+public class APILayout {
+
+
+}
