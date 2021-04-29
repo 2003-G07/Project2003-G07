360410e8a346912dd679563246895a23b991747d
Salah Abdinoor <Salah.abdinoor@hotmail.com>
2021-04-29 17:31:37 +0200

    Beställningar kan nu skickas in, dock med bara en product

--- src/main/java/com/example/demo1/DemoApplication.java
+++ src/main/java/com/example/demo1/DemoApplication.java
@@ -1,7 +1,11 @@
 package com.example.demo1;
 
+import com.example.demo1.models.Product;
+import com.example.demo1.repositories.ProductRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class DemoApplication {
@@ -9,9 +13,8 @@ public class DemoApplication {
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
     }
-}
 
-/*
+    /*
     @Bean
     public CommandLineRunner demo(ProductRepository repository) {
         return (args -> {
@@ -20,4 +23,7 @@ public class DemoApplication {
             repository.save(new Product("Prod 3", 11, "https://picsum.photos/500?random=3", "Ett svart te smaksatt med bergamott.", 160, "Dryck", true));
         });
     }
- */
+
+     */
+
+}
