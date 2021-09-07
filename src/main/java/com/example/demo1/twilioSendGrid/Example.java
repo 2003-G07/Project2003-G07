5c8bd8e374aa82f5f123f24be2c4048f8db509fb
Salah Abdinoor <Salah.abdinoor@hotmail.com>
2021-09-07 14:51:20 +0200

    Example fil

--- src/main/java/com/example/demo1/twilioSendGrid/Example.java
+++ src/main/java/com/example/demo1/twilioSendGrid/Example.java
@@ -1,18 +1,27 @@
 package com.example.demo1.twilioSendGrid;// using SendGrid's Java Library
 // https://github.com/sendgrid/sendgrid-java
+
+import com.sendgrid.Method;
+import com.sendgrid.Request;
+import com.sendgrid.Response;
+import com.sendgrid.SendGrid;
+import com.sendgrid.helpers.mail.Mail;
+import com.sendgrid.helpers.mail.objects.Content;
+import com.sendgrid.helpers.mail.objects.Email;
+
 import java.io.IOException;
 
 public class Example {
 
     public static void main(String[] args) throws IOException {
 
-        Email from = new Email("test@example.com");
+        Email from = new Email("henke_e_96@hotmail.com");
         String subject = "Sending with SendGrid is Fun";
-        Email to = new Email("test@example.com");
+        Email to = new Email("henke_e_96@hotmail.com");
         Content content = new Content("text/plain", "and easy to do anywhere, even with Java");
         Mail mail = new Mail(from, subject, to, content);
 
-        SendGrid sg = new SendGrid(System.getenv("SENDGRID_API_KEY"));
+        SendGrid sg = new SendGrid("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");
         Request request = new Request();
         try {
             request.setMethod(Method.POST);
