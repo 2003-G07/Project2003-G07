1ce2acee049155d6d221b0a1ae52476e85ab54ec
”Daniel <dannekun@hotmail.com>
2021-09-14 17:07:14 +0200

    ready for test

--- src/main/java/com/example/demo1/DemoApplication.java
+++ src/main/java/com/example/demo1/DemoApplication.java
@@ -2,9 +2,6 @@ package com.example.demo1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.client.RestTemplate;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 
 @SpringBootApplication
